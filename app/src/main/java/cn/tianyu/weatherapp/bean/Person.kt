5cec44e36fc2e77b124e9d55888824fa61a91943
countBunny <happy_zoo@126.com>
2018-04-11 15:44:55 +0800

    acknowledge attrs in kotlin

--- app/src/main/java/cn/tianyu/weatherapp/bean/Person.kt
+++ app/src/main/java/cn/tianyu/weatherapp/bean/Person.kt
@@ -3,11 +3,20 @@ package cn.tianyu.weatherapp.bean
 import android.content.Context
 import android.widget.Toast
 
-class Person(name: String, surname: String) : Animal(name) {
+class Person(name: String, surname: String = "") : Animal(name) {
+
+    var name:String = ""
 
     init {
         //构造函数可以在这里写
+        this.name = name
     }
+
+    /*get() = field.toUpperCase()
+
+    set(value){
+        field = "Name:$value"
+    }*/
 }
 
 open class Animal(name: String)
@@ -20,4 +29,13 @@ fun adddirect(x: Int, y: Int): Int = x + y
 
 fun toast(context: Context, message:String, length:Int=Toast.LENGTH_SHORT){
     Toast.makeText(context, message, length).show()
+}
+
+fun practice1(){
+    var i:Int = 7
+    val d:Double = i.toDouble()
+    val c: Char='c'
+    i = c.toInt()
+    val person = Person("name")
+    val name = person.name
 }
